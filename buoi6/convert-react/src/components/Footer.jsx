6222d9bf609c1a2e8c66e4f59672d54f7e724feb
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2023-06-21 20:31:23 +0700

    Update layout browser

--- buoi6/convert-react/src/components/Footer.jsx
+++ buoi6/convert-react/src/components/Footer.jsx
@@ -1,7 +1,19 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved.
+              
+                <br/>Design: Convert Web Tĩnh</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
